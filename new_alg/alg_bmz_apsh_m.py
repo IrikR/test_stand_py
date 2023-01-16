90afb0084a9969cf962e36d453e8157987ea76c3
IrikSafin <osiris365@gmail.com>
2023-01-16 13:38:52 +0500

    метод переработан согласно новых и измененных методов

--- new_alg/alg_bmz_apsh_m.py
+++ new_alg/alg_bmz_apsh_m.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 !!! НОВЫЙ НЕ ОБКАТАНЫЙ !!!
 
@@ -11,42 +9,38 @@
 
 """
 
-import sys
-import logging
+__all__ = ["TestBMZAPSHM"]
 
+import logging
+import sys
 from time import sleep
 
-from .general_func.exception import *
 from .general_func.database import *
-from .general_func.modbus import *
+from .general_func.exception import *
+from .general_func.opc_full import ConnectOPC
 from .general_func.procedure import *
-from .general_func.subtest import ReadOPCServer
-from .general_func.reset import ResetRelay, ResetProtection
+from .general_func.reset import ResetProtection, ResetRelay
 from .general_func.subtest import ProcedureFull
-from .gui.msgbox_1 import *
 from .general_func.utils import CLILog
-
-
-__all__ = ["TestBMZAPSHM"]
+from .gui.msgbox_1 import *
 
 
 class TestBMZAPSHM:
 
     def __init__(self):
+        self.conn_opc = ConnectOPC()
         self.proc = Procedure()
         self.proc_full = ProcedureFull()
         self.reset_relay = ResetRelay()
         self.reset_protect = ResetProtection()
-        self.ctrl_kl = CtrlKL()
         self.mysql_conn = MySQLConnect()
-        self.di_read = ReadOPCServer()
-        self.cli_log = CLILog(True, __name__)
+        self.cli_log = CLILog("debug", __name__)
 
         self.ust_1: float = 85.6
 
         self.coef_volt: float = 0.0
         self.health_flag: bool = False
-        
+
         self.msg_1 = "Убедитесь в отсутствии блоков во всех испытательных разъемах. " \
                      "Вставьте блок в соответствующий испытательный разъем»"
 
@@ -64,18 +58,20 @@ class TestBMZAPSHM:
         """
         Тест 1. Проверка исходного состояния блока:
         """
+        self.cli_log.lev_info(f"старт теста {__doc__}", "skyblue")
         if my_msg(self.msg_1):
             pass
         else:
             return False
-        self.logger.debug("тест 1")
-        self.ctrl_kl.ctrl_relay('KL21', True)
+        self.logger.debug("тест 1.0")
+        self.cli_log.lev_info("тест 1", "skyblue")
+        self.conn_opc.ctrl_relay('KL21', True)
         self.logger.debug('включен KL21')
         self.reset_protect.sbros_zashit_kl30()
-        if self.di_read.subtest_4di(test_num=1, subtest_num=1.0,
-                                    err_code_a=347, err_code_b=348, err_code_c=349, err_code_d=350,
-                                    position_a=False, position_b=True, position_c=False, position_d=True,
-                                    di_a='in_a1', di_b='in_a5', di_c='in_a2', di_d='in_a6'):
+        if self.conn_opc.subtest_read_di(test_num=1, subtest_num=1.0,
+                                    err_code=[347, 348, 349, 350],
+                                    position_inp=[False, True, False, True],
+                                    di_xx=['inp_01', 'inp_05', 'inp_02', 'inp_06']):
             return True
         return False
 
@@ -84,6 +80,7 @@ class TestBMZAPSHM:
         1.1.2. Проверка отсутствия короткого замыкания на входе измерительной части блока:
         1.2. Определение коэффициента Кс отклонения фактического напряжения от номинального
         """
+        self.cli_log.lev_info("тест 1.1", "skyblue")
         if self.proc_full.procedure_1_full(test_num=1, subtest_num=1.1, coef_min_volt=0.9):
             self.coef_volt = self.proc_full.procedure_2_full(test_num=1, subtest_num=1.2)
             return True
@@ -94,6 +91,7 @@ class TestBMZAPSHM:
         Тест 2. Проверка работы 1 канала блока
         """
         self.logger.debug("старт теста 2.0")
+        self.cli_log.lev_info("тест 2.0", "skyblue")
         if self.proc.procedure_x4_to_x5(coef_volt=self.coef_volt, setpoint_volt=self.ust_1):
             return True
         self.mysql_conn.mysql_ins_result('неисправен', '2')
@@ -103,21 +101,22 @@ class TestBMZAPSHM:
         """
         2.1.  Проверка срабатывания блока от сигнала нагрузки:
         """
+        self.cli_log.lev_info("тест 2.1", "skyblue")
         self.logger.debug("старт теста 2.1")
-        self.ctrl_kl.ctrl_relay('KL63', True)
+        self.conn_opc.ctrl_relay('KL63', True)
         self.logger.debug('включен KL63')
         sleep(3)
         self.logger.debug("таймаут 3 сек")
-        self.cli_log.log_msg("таймаут 3 сек", "gray")
-        self.ctrl_kl.ctrl_relay('KL63', False)
+        self.cli_log.lev_debug("таймаут 3 сек", "gray")
+        self.conn_opc.ctrl_relay('KL63', False)
         self.logger.debug('отключен KL63')
         sleep(1)
         self.logger.debug("таймаут 1 сек")
-        self.cli_log.log_msg("таймаут 1 сек", "gray")
-        if self.di_read.subtest_4di(test_num=2, subtest_num=2.1,
-                                    err_code_a=352, err_code_b=353, err_code_c=354, err_code_d=355,
-                                    position_a=True, position_b=False, position_c=False, position_d=True,
-                                    di_a='in_a1', di_b='in_a5', di_c='in_a2', di_d='in_a6'):
+        self.cli_log.lev_debug("таймаут 1 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=2, subtest_num=2.1,
+                                    err_code=[352, 353, 354, 355],
+                                    position_inp=[True, False, False, True],
+                                    di_xx=['inp_01', 'inp_05', 'inp_02', 'inp_06']):
             self.reset_relay.stop_procedure_3()
             return True
         self.reset_relay.stop_procedure_3()
@@ -128,11 +127,12 @@ class TestBMZAPSHM:
         2.2. Сброс защит после проверки
         """
         self.logger.debug("старт теста 2.2")
+        self.cli_log.lev_info("тест 2.2", "skyblue")
         self.reset_protect.sbros_zashit_kl30()
-        if self.di_read.subtest_4di(test_num=2, subtest_num=2.2,
-                                    err_code_a=356, err_code_b=357, err_code_c=358, err_code_d=359,
-                                    position_a=False, position_b=True, position_c=False, position_d=True,
-                                    di_a='in_a1', di_b='in_a5', di_c='in_a2', di_d='in_a6'):
+        if self.conn_opc.subtest_read_di(test_num=2, subtest_num=2.2,
+                                    err_code=[356, 357, 358, 359],
+                                    position_inp=[False, True, False, True],
+                                    di_xx=['inp_01', 'inp_05', 'inp_02', 'inp_06']):
             return True
         return False
 
@@ -141,7 +141,8 @@ class TestBMZAPSHM:
         Тест 3. Проверка работы 2 канала блока
         """
         self.logger.debug("старт теста 3.0")
-        self.ctrl_kl.ctrl_relay('KL73', True)
+        self.cli_log.lev_info("тест 3.0", "skyblue")
+        self.conn_opc.ctrl_relay('KL73', True)
         self.logger.debug('включен KL73')
         if self.proc.procedure_x4_to_x5(coef_volt=self.coef_volt, setpoint_volt=self.ust_1):
             return True
@@ -149,20 +150,21 @@ class TestBMZAPSHM:
         return False
 
     def st_test_31(self) -> bool:
-        self.ctrl_kl.ctrl_relay('KL63', True)
+        self.cli_log.lev_info("тест 3.1", "skyblue")
+        self.conn_opc.ctrl_relay('KL63', True)
         self.logger.debug('включен KL63')
         sleep(3)
         self.logger.debug("таймаут 3 сек")
-        self.cli_log.log_msg("таймаут 3 сек", "gray")
-        self.ctrl_kl.ctrl_relay('KL63', False)
+        self.cli_log.lev_debug("таймаут 3 сек", "gray")
+        self.conn_opc.ctrl_relay('KL63', False)
         self.logger.debug('отключен KL63')
         sleep(1)
         self.logger.debug("таймаут 1 сек")
-        self.cli_log.log_msg("таймаут 1 сек", "gray")
-        if self.di_read.subtest_4di(test_num=3, subtest_num=3.1,
-                                    err_code_a=360, err_code_b=361, err_code_c=362, err_code_d=363,
-                                    position_a=False, position_b=True, position_c=True, position_d=False,
-                                    di_a='in_a1', di_b='in_a5', di_c='in_a2', di_d='in_a6'):
+        self.cli_log.lev_debug("таймаут 1 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=3, subtest_num=3.1,
+                                    err_code=[360, 361, 362, 363],
+                                    position_inp=[False, True, True, False],
+                                    di_xx=['inp_01', 'inp_05', 'inp_02', 'inp_06']):
             self.reset_relay.stop_procedure_3()
             return True
         self.reset_relay.stop_procedure_3()
@@ -173,14 +175,15 @@ class TestBMZAPSHM:
         3.2. Сброс защит после проверки
         """
         self.logger.debug("старт теста 3.2")
+        self.cli_log.lev_info("тест 3.2", "skyblue")
         self.reset_protect.sbros_zashit_kl30()
         sleep(1)
         self.logger.debug("таймаут 1 сек")
-        self.cli_log.log_msg("таймаут 1 сек", "gray")
-        if self.di_read.subtest_4di(test_num=3, subtest_num=3.2,
-                                    err_code_a=364, err_code_b=365, err_code_c=366, err_code_d=367,
-                                    position_a=False, position_b=True, position_c=False, position_d=True,
-                                    di_a='in_a1', di_b='in_a5', di_c='in_a2', di_d='in_a6'):
+        self.cli_log.lev_debug("таймаут 1 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=3, subtest_num=3.2,
+                                    err_code=[364, 365, 366, 367],
+                                    position_inp=[False, True, False, True],
+                                    di_xx=['inp_01', 'inp_05', 'inp_02', 'inp_06']):
             return True
         return False
 
@@ -202,27 +205,32 @@ class TestBMZAPSHM:
             if test and not health_flag:
                 self.mysql_conn.mysql_block_good()
                 self.logger.debug('Блок исправен')
-                self.cli_log.log_msg('Блок исправен', 'green')
+                self.cli_log.lev_info('Блок исправен', 'green')
                 my_msg('Блок исправен', 'green')
             else:
                 self.mysql_conn.mysql_block_bad()
                 self.logger.debug('Блок неисправен')
-                self.cli_log.log_msg('Блок неисправен', 'red')
+                self.cli_log.lev_warning('Блок неисправен', 'red')
                 my_msg('Блок неисправен', 'red')
         except OSError:
             self.logger.debug("ошибка системы")
-            self.cli_log.log_msg("ошибка системы", 'red')
+            self.cli_log.lev_warning("ошибка системы", 'red')
             my_msg("ошибка системы", 'red')
         except SystemError:
             self.logger.debug("внутренняя ошибка")
-            self.cli_log.log_msg("внутренняя ошибка", 'red')
+            self.cli_log.lev_warning("внутренняя ошибка", 'red')
             my_msg("внутренняя ошибка", 'red')
         except ModbusConnectException as mce:
             self.logger.debug(f'{mce}')
-            self.cli_log.log_msg(f'{mce}', 'red')
+            self.cli_log.lev_warning(f'{mce}', 'red')
             my_msg(f'{mce}', 'red')
+        except HardwareException as hwe:
+            self.logger.debug(f'{hwe}')
+            self.cli_log.lev_warning(f'{hwe}', 'red')
+            my_msg(f'{hwe}', 'red')
         finally:
-            self.reset_relay.reset_all()
+            self.conn_opc.full_relay_off()
+            self.conn_opc.opc_close()
             sys.exit()
 
 
