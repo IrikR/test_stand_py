ec6d4e2fa52273fe67faafe247560456a70af0a8
IrikSafin <osiris365@gmail.com>
2023-01-16 11:37:30 +0500

    метод переработан согласно новых и измененных методов

--- new_alg/alg_bki_6_3sh.py
+++ new_alg/alg_bki_6_3sh.py
@@ -1,42 +1,37 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 !!! НОВЫЙ НЕ ОБКАТАНЫЙ !!!
 
 Алгоритм проверки
+
 Тип блока: БКИ-6-3Ш
 Производитель:
 
 """
 
-import sys
-import logging
+__all__ = ["TestBKI6"]
 
+import logging
+import sys
 from time import sleep
 
-from .general_func.exception import *
 from .general_func.database import *
-from .general_func.modbus import *
-from .general_func.resistance import Resistor
+from .general_func.exception import *
+from .general_func.opc_full import ConnectOPC
 from .general_func.reset import ResetRelay
-from .general_func.subtest import ReadOPCServer
-from .gui.msgbox_1 import *
+from .general_func.resistance import Resistor
 from .general_func.utils import CLILog
-
-__all__ = ["TestBKI6"]
+from .gui.msgbox_1 import *
 
 
 class TestBKI6:
 
     def __init__(self):
+        self.conn_opc = ConnectOPC()
         self.resist = Resistor()
-        self.ctrl_kl = CtrlKL()
-        self.di_read = DIRead()
         self.mysql_conn = MySQLConnect()
-        self.di_read_full = ReadOPCServer()
         self.reset_relay = ResetRelay()
-        self.cli_log = CLILog(True, __name__)
+        self.cli_log = CLILog("debug", __name__)
 
         self.msg_1 = 'Убедитесь в отсутствии других блоков или соединительных кабелей в панели разъемов А'
         self.msg_2 = 'Подключите в разъем, расположенный на панели разъемов А ' \
@@ -56,6 +51,7 @@ class TestBKI6:
         """
         Тест 1. Проверка исходного состояния контактов блока при отсутствии напряжения питания
         """
+        self.cli_log.lev_info(f"старт теста {__doc__}", "skyblue")
         if my_msg(self.msg_1):
             if my_msg(self.msg_2):
                 pass
@@ -63,13 +59,12 @@ class TestBKI6:
                 return False
         else:
             return False
-        self.ctrl_kl.ctrl_relay('KL22', True)
+        self.conn_opc.ctrl_relay('KL22', True)
         sleep(3)
-        if self.di_read_full.subtest_5di(test_num=1, subtest_num=1.0, err_code_a=123, err_code_b=123, err_code_c=124,
-                                         err_code_d=125, err_code_e=125, position_a=False, position_b=True,
-                                         position_c=True,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        if self.conn_opc.subtest_read_di(test_num=1, subtest_num=1.0,
+                                         err_code=[123, 123, 124, 125, 125],
+                                         position_inp=[False, True, True, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
@@ -77,7 +72,7 @@ class TestBKI6:
         """
         Тест 2. Проверка работы контактов блока при подаче питания на блок и отсутствии утечки
         """
-        self.ctrl_kl.ctrl_relay('KL21', True)
+        self.conn_opc.ctrl_relay('KL21', True)
         if self.sub_test(test_num=2, subtest_num=2.0, iteration=20):
             return True
         return False
@@ -86,11 +81,10 @@ class TestBKI6:
         """
         2.1. Проверка установившегося состояния контактов по истечению 20 сек
         """
-        if self.di_read_full.subtest_5di(test_num=2, subtest_num=2.1, err_code_a=126, err_code_b=126, err_code_c=127,
-                                         err_code_d=128, err_code_e=128, position_a=False, position_b=True,
-                                         position_c=True,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        if self.conn_opc.subtest_read_di(test_num=2, subtest_num=2.1,
+                                         err_code=[126, 126, 127, 128, 128],
+                                         position_inp=[False, True, True, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
@@ -98,20 +92,19 @@ class TestBKI6:
         """
         Тест 3. Проверка работы контактов реле К4 «Блокировка ВКЛ».
         """
-        self.ctrl_kl.ctrl_relay('KL36', True)
+        self.conn_opc.ctrl_relay('KL36', True)
         sleep(1)
         self.logger.debug("таймаут 1 сек")
-        self.cli_log.log_msg("таймаут 1 сек", "gray")
-        if self.di_read_full.subtest_5di(test_num=3, subtest_num=3.0, err_code_a=129, err_code_b=129, err_code_c=130,
-                                         err_code_d=131, err_code_e=131, position_a=False, position_b=True,
-                                         position_c=True,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        self.cli_log.lev_debug("таймаут 1 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=3, subtest_num=3.0,
+                                         err_code=[129, 129, 130, 131, 131],
+                                         position_inp=[False, True, True, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
     def st_test_31(self) -> bool:
-        self.ctrl_kl.ctrl_relay('KL36', False)
+        self.conn_opc.ctrl_relay('KL36', False)
         if self.sub_test(test_num=3, subtest_num=3.1, iteration=40):
             return True
         return False
@@ -121,11 +114,10 @@ class TestBKI6:
 
         :return: bool
         """
-        if self.di_read_full.subtest_5di(test_num=3, subtest_num=3.2, err_code_a=132, err_code_b=132, err_code_c=133,
-                                         err_code_d=134, err_code_e=134, position_a=False, position_b=True,
-                                         position_c=True,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        if self.conn_opc.subtest_read_di(test_num=3, subtest_num=3.2,
+                                         err_code=[132, 132, 133, 134, 134],
+                                         position_inp=[False, True, True, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
@@ -133,16 +125,15 @@ class TestBKI6:
         """
         Тест 4. Проверка работы контактов реле К6 «Срабатывание БКИ»
         """
-        self.ctrl_kl.ctrl_relay('KL22', False)
+        self.conn_opc.ctrl_relay('KL22', False)
         self.resist.resist_kohm(30)
         sleep(10)
         self.logger.debug("таймаут 10 сек")
-        self.cli_log.log_msg("таймаут 10 сек", "gray")
-        if self.di_read_full.subtest_5di(test_num=4, subtest_num=4.0, err_code_a=135, err_code_b=135, err_code_c=136,
-                                         err_code_d=137, err_code_e=137, position_a=False, position_b=True,
-                                         position_c=False,
-                                         position_d=True, position_e=False, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        self.cli_log.lev_debug("таймаут 10 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=4, subtest_num=4.0,
+                                         err_code=[135, 135, 136, 137, 137],
+                                         position_inp=[False, True, False, True, False],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
@@ -153,12 +144,11 @@ class TestBKI6:
         self.resist.resist_kohm(590)
         sleep(2)
         self.logger.debug("таймаут 2 сек")
-        self.cli_log.log_msg("таймаут 2 сек", "gray")
-        if self.di_read_full.subtest_5di(test_num=4, subtest_num=4.1, err_code_a=138, err_code_b=138, err_code_c=139,
-                                         err_code_d=140, err_code_e=140, position_a=False, position_b=True,
-                                         position_c=False,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        self.cli_log.lev_debug("таймаут 2 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=4, subtest_num=4.1,
+                                         err_code=[138, 138, 139, 140, 140],
+                                         position_inp=[False, True, False, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
@@ -166,28 +156,26 @@ class TestBKI6:
         """
         Тест 5. Проверка исправности контактов реле К5 «Срабатывание БКИ на сигнал
         """
-        self.ctrl_kl.ctrl_relay('KL22', True)
+        self.conn_opc.ctrl_relay('KL22', True)
         sleep(2)
         self.logger.debug("таймаут 2 сек")
-        self.cli_log.log_msg("таймаут 2 сек", "gray")
-        if self.di_read_full.subtest_5di(test_num=5, subtest_num=5.0, err_code_a=141, err_code_b=141, err_code_c=142,
-                                         err_code_d=143, err_code_e=143, position_a=False, position_b=True,
-                                         position_c=True,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        self.cli_log.lev_debug("таймаут 2 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=5, subtest_num=5.0,
+                                         err_code=[141, 141, 142, 143, 143],
+                                         position_inp=[False, True, True, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
     def st_test_51(self) -> bool:
-        self.ctrl_kl.ctrl_relay('KL22', False)
+        self.conn_opc.ctrl_relay('KL22', False)
         sleep(5)
         self.logger.debug("таймаут 5 сек")
-        self.cli_log.log_msg("таймаут 5 сек", "gray")
-        if self.di_read_full.subtest_5di(test_num=5, subtest_num=5.1, err_code_a=144, err_code_b=144, err_code_c=145,
-                                         err_code_d=146, err_code_e=146, position_a=False, position_b=True,
-                                         position_c=False,
-                                         position_d=False, position_e=True, di_a="in_a1", di_b='in_a7', di_c='in_a6',
-                                         di_d='in_a4', di_e='in_a5'):
+        self.cli_log.lev_debug("таймаут 5 сек", "gray")
+        if self.conn_opc.subtest_read_di(test_num=5, subtest_num=5.1,
+                                         err_code=[144, 144, 145, 146, 146],
+                                         position_inp=[False, True, False, False, True],
+                                         di_xx=['inp_01', 'inp_07', 'inp_06', 'inp_04', 'inp_05']):
             return True
         return False
 
@@ -203,14 +191,14 @@ class TestBKI6:
         self.logger.debug(f"тест {test_num}, подтест {subtest_num}")
         self.mysql_conn.mysql_ins_result(f"идёт подтест {subtest_num}", f"{test_num}")
         k1 = 0
-        in_a1, in_a7 = self.di_read.di_read('in_a1', 'in_a7')
-        self.logger.debug(f"положение выходов блока: {in_a1 = } is True, {in_a7 = } is False")
-        while in_a1 is False and in_a7 is True and k1 <= iteration:
+        inp_01, inp_07 = self.conn_opc.simplified_read_di(['inp_01', 'inp_07'])
+        self.logger.debug(f"положение выходов блока: {inp_01 = } is True, {inp_07 = } is False")
+        while inp_01 is False and inp_07 is True and k1 <= iteration:
             sleep(0.2)
-            in_a1, in_a7 = self.di_read.di_read('in_a1', 'in_a7')
-            self.logger.debug(f"итерация {k1} положение выходов блока: {in_a1 = } is True, {in_a7 = } is False")
+            inp_01, inp_07 = self.conn_opc.simplified_read_di(['inp_01', 'inp_07'])
+            self.logger.debug(f"итерация {k1} положение выходов блока: {inp_01 = } is True, {inp_07 = } is False")
             k1 += 1
-        if in_a1 is True and in_a7 is False:
+        if inp_01 is True and inp_07 is False:
             self.logger.debug(f'подтест {subtest_num} положение выходов соответствует')
             self.mysql_conn.mysql_add_message(f'подтест {subtest_num} положение выходов соответствует')
         else:
@@ -220,13 +208,13 @@ class TestBKI6:
             return False
 
         k2 = 0
-        in_a1, in_a7 = self.di_read.di_read('in_a1', 'in_a7')
-        while in_a1 is True and in_a7 is False and k2 <= iteration:
+        inp_01, inp_07 = self.conn_opc.simplified_read_di(['inp_01', 'inp_07'])
+        while inp_01 is True and inp_07 is False and k2 <= iteration:
             sleep(0.2)
-            in_a1, in_a7 = self.di_read.di_read('in_a1', 'in_a7')
-            self.logger.debug(f"итерация {k2} положение выходов блока: {in_a1 = } is False, {in_a7 = } is True")
+            inp_01, inp_07 = self.conn_opc.simplified_read_di(['inp_01', 'inp_07'])
+            self.logger.debug(f"итерация {k2} положение выходов блока: {inp_01 = } is False, {inp_07 = } is True")
             k2 += 1
-        if in_a1 is False and in_a7 is True:
+        if inp_01 is False and inp_07 is True:
             self.logger.debug(f'подтест {subtest_num} положение выходов соответствует')
             self.mysql_conn.mysql_add_message(f'подтест {subtest_num} положение выходов соответствует')
             return True
@@ -255,27 +243,28 @@ class TestBKI6:
             if self.st_test_bki_6_3sh():
                 self.mysql_conn.mysql_block_good()
                 self.logger.debug('Блок исправен')
-                self.cli_log.log_msg('Блок исправен', 'green')
+                self.cli_log.lev_info('Блок исправен', 'green')
                 my_msg('Блок исправен', 'green')
             else:
                 self.mysql_conn.mysql_block_bad()
                 self.logger.debug('Блок неисправен')
-                self.cli_log.log_msg('Блок неисправен', 'red')
+                self.cli_log.lev_warning('Блок неисправен', 'red')
                 my_msg('Блок неисправен', 'red')
         except OSError:
             self.logger.debug("ошибка системы")
-            self.cli_log.log_msg("ошибка системы", 'red')
+            self.cli_log.lev_warning("ошибка системы", 'red')
             my_msg("ошибка системы", 'red')
         except SystemError:
             self.logger.debug("внутренняя ошибка")
-            self.cli_log.log_msg("внутренняя ошибка", 'red')
+            self.cli_log.lev_warning("внутренняя ошибка", 'red')
             my_msg("внутренняя ошибка", 'red')
         except ModbusConnectException as mce:
             self.logger.debug(f'{mce}')
-            self.cli_log.log_msg(f'{mce}', 'red')
+            self.cli_log.lev_warning(f'{mce}', 'red')
             my_msg(f'{mce}', 'red')
         finally:
-            self.reset_relay.reset_all()
+            self.conn_opc.full_relay_off()
+            self.conn_opc.opc_close()
             sys.exit()
 
 
