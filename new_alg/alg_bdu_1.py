6017e8803ce1d340be1fb026e3ba416871327b4e
IrikSafin <osiris365@gmail.com>
2023-01-21 21:51:28 +0500

    дополнено логирование

--- new_alg/alg_bdu_1.py
+++ new_alg/alg_bdu_1.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """
 !!! НА ИСПЫТАНИИ !!!
 
@@ -62,6 +61,7 @@ class TestBDU1:
         :return: Bool
         """
         self.cli_log.lev_info(f"старт теста {__doc__}", "skyblue")
+        self.logger.debug(f"старт теста {__doc__}")
         if self.conn_opc.subtest_read_di(test_num=1, subtest_num=1.0, err_code=[30], di_xx=['inp_01'],
                                          position_inp=[False]):
             return True
@@ -72,8 +72,11 @@ class TestBDU1:
         Тест-2 Проверка включения/отключения блока от кнопки пуск.
         :return: Bool
         """
-        self.cli_log.lev_info("Тест 2", "gray")
+        self.logger.debug(f"теста 2.0")
+        self.cli_log.lev_info("теста 2.0", "gray")
         self.conn_opc.ctrl_relay('KL2', True)
+        self.cli_log.lev_info("Включено реле KL2", "blue")
+        self.logger.debug("Включено реле KL2")
         sleep(3)
         self.logger.debug("таймаут 3 сек")
         self.cli_log.lev_debug("таймаут 3 сек", "gray")
@@ -88,12 +91,15 @@ class TestBDU1:
             Код ошибки 21 – Сообщение: Блок не исправен. Нет срабатывания блока от кнопки Пуск.
         :return: Bool
         """
+        self.logger.debug(f"теста 2.1")
         self.cli_log.lev_info("Тест 2.1", "gray")
         self.resist.resist_ohm(10)
         sleep(3)
         self.logger.debug("таймаут 3 сек")
         self.cli_log.lev_debug("таймаут 3 сек", "gray")
         self.conn_opc.ctrl_relay('KL12', True)
+        self.cli_log.lev_info("Включено реле KL12", "blue")
+        self.logger.debug("Включено реле KL12")
         sleep(3)
         self.logger.debug("таймаут 3 сек")
         self.cli_log.lev_debug("таймаут 3 сек", "gray")
@@ -108,11 +114,14 @@ class TestBDU1:
             Код ошибки	23	–	Сообщение	«Блок не исправен. Блок не выключается от кнопки «Стоп».
         :return: Bool
         """
+        self.logger.debug(f"теста 2.2")
         self.cli_log.lev_info("Тест 2.2", "gray")
         sleep(3)
         self.logger.debug("таймаут 3 сек")
         self.cli_log.lev_debug("таймаут 3 сек", "gray")
         self.conn_opc.ctrl_relay('KL12', False)
+        self.cli_log.lev_info("Отключено реле KL2", "blue")
+        self.logger.debug("Отключено реле KL2")
         sleep(3)
         self.logger.debug("таймаут 3 сек")
         self.cli_log.lev_debug("таймаут 3 сек", "gray")
@@ -129,17 +138,22 @@ class TestBDU1:
 
         :return: Bool
         """
+        self.logger.debug(f"теста 3.0")
         self.cli_log.lev_info("Тест 3", "gray")
         sleep(3)
         self.logger.debug("таймаут 3 сек")
         self.cli_log.lev_debug("таймаут 3 сек", "gray")
         self.conn_opc.ctrl_relay('KL12', True)
+        self.cli_log.lev_info("Включено реле KL12", "blue")
+        self.logger.debug("Включено реле KL12")
         sleep(0.5)
         self.logger.debug("таймаут 0.5 сек")
         self.cli_log.lev_debug("таймаут 0.5 сек", "gray")
         # Отключаем KL5, KL8 для формирования 35 Ом
         self.conn_opc.ctrl_relay('KL5', False)
         self.conn_opc.ctrl_relay('KL8', False)
+        self.cli_log.lev_info("Отключено реле KL5, KL8", "blue")
+        self.logger.debug("Отключено реле KL5, KL8")
         sleep(1)
         self.logger.debug("таймаут 1 сек")
         self.cli_log.lev_debug("таймаут 1 сек", "gray")
@@ -155,12 +169,23 @@ class TestBDU1:
             сопротивлении цепи заземления более 50 Ом».
         :return: Bool
         """
+        self.logger.debug(f"теста 4.0")
         self.cli_log.lev_info("Тест 4", "gray")
         self.conn_opc.ctrl_relay('KL7', False)
+        self.cli_log.lev_info("Отключено реле KL7", "blue")
+        self.logger.debug("Отключено реле KL7")
         self.conn_opc.ctrl_relay('KL9', False)
+        self.cli_log.lev_info("Отключено реле KL9", "blue")
+        self.logger.debug("Отключено реле KL9")
         self.conn_opc.ctrl_relay('KL4', True)
+        self.cli_log.lev_info("Включено реле KL4", "blue")
+        self.logger.debug("Включено реле KL4")
         self.conn_opc.ctrl_relay('KL6', True)
+        self.cli_log.lev_info("Включено реле KL6", "blue")
+        self.logger.debug("Включено реле KL6")
         self.conn_opc.ctrl_relay('KL10', True)
+        self.cli_log.lev_info("Включено реле KL10", "blue")
+        self.logger.debug("Включено реле KL10")
         sleep(2)
         self.logger.debug("таймаут 2 сек")
         self.cli_log.lev_debug("таймаут 2 сек", "gray")
@@ -177,16 +202,21 @@ class TestBDU1:
                 при замыкании проводов цепей ДУ».
         :return: Bool
         """
+        self.logger.debug(f"теста 5.0")
         self.cli_log.lev_info("Тест 5", "gray")
         self.resist.resist_ohm(10)
         sleep(1)
         self.logger.debug("таймаут 1 сек")
         self.cli_log.lev_debug("таймаут 1 сек", "gray")
         self.conn_opc.ctrl_relay('KL12', True)
+        self.cli_log.lev_info("Включено реле KL12", "blue")
+        self.logger.debug("Включено реле KL12")
         sleep(0.5)
         self.logger.debug("таймаут 0.5 сек")
         self.cli_log.lev_debug("таймаут 0.5 сек", "gray")
         self.conn_opc.ctrl_relay('KL11', True)
+        self.cli_log.lev_info("Включено реле KL11", "blue")
+        self.logger.debug("Включено реле KL11")
         sleep(1)
         self.logger.debug("таймаут 1 сек")
         self.cli_log.lev_debug("таймаут 1 сек", "gray")
@@ -195,6 +225,8 @@ class TestBDU1:
             self.conn_opc.ctrl_relay('KL12', False)
             self.conn_opc.ctrl_relay('KL11', False)
             self.conn_opc.ctrl_relay('KL1', False)
+            self.cli_log.lev_info("Отключены реле KL12, KL11, KL1", "blue")
+            self.logger.debug("Отключены реле KL12, KL11, KL1")
             return True
         return False
 
@@ -203,16 +235,21 @@ class TestBDU1:
         Тест 6. Защита от потери управляемости при обрыве проводов ДУ
         :return: Bool
         """
+        self.logger.debug(f"теста 6.0")
         self.cli_log.lev_info("Тест 6", "gray")
         self.resist.resist_ohm(10)
         sleep(1)
         self.logger.debug("таймаут 1 сек")
         self.cli_log.lev_debug("таймаут 1 сек", "gray")
         self.conn_opc.ctrl_relay('KL12', True)
+        self.cli_log.lev_info("Включено реле KL12", "blue")
+        self.logger.debug("Включено реле KL12")
         sleep(1)
         self.logger.debug("таймаут 1 сек")
         self.cli_log.lev_debug("таймаут 1 сек", "gray")
         self.conn_opc.ctrl_relay('KL12', False)
+        self.cli_log.lev_info("Отключено реле KL12", "blue")
+        self.logger.debug("Отключено реле KL12")
         sleep(2)
         self.logger.debug("таймаут 2 сек")
         self.cli_log.lev_debug("таймаут 2 сек", "gray")
