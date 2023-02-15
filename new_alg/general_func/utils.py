e87eac8da0fa62f29d0bcaeaa17bb11c0c56fce8
IrikSafin <osiris365@gmail.com>
2023-02-15 22:55:45 +0500

    полностью переделаны функции печати в CLI

--- new_alg/general_func/utils.py
+++ new_alg/general_func/utils.py
@@ -18,6 +18,14 @@ class CLILog:
         Фиолетовый  35      45
         Бирюзовый   36      46
         Белый       37      47
+
+        red - для неисправности
+        orange - для измерений
+        blue - для управления реле
+        green - для исправности
+        purple - для процедур
+        skyblue - для сухих контактов
+        gray - для основного текста
     """
 
     def __init__(self, log=None, name=__name__):
@@ -26,72 +34,52 @@ class CLILog:
         kernel32 = ctypes.windll.kernel32
         kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
-    def lev_debug(self, msg: str, msg_color: [int, str]):
+    def color_print(self, *args):
+        color_level: dict = {"black": 30,
+                             "red": 31,
+                             "green": 32,
+                             "orange": 33,
+                             "blue": 34,
+                             "purple": 35,
+                             "skyblue": 36,
+                             "gray": 37,
+                             "reset": 0}
+        msg, color_msg = args
+        color = color_level[color_msg] if color_msg in color_level else 0
+        print(f"\033[37m [{datetime.time(datetime.now())} [{self.log}] {self.name}] : \033[{color}m {msg}")
+
+    def lev_debug(self, msg: str, msg_color: str) -> None:
+        """
+        Функция вывода сообщений в консоль уровня DEBUG
+        params msg: сообщение
+        params msg_color: цвет сообщения
+        return
+        """
         if self.log == "debug" or self.log is True:
-            if msg_color == 7 or msg_color == 'gray':
-                # серый
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[37m {msg}")
-            else:
-                # черный, если пришел неизвестный аргумент
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[0;0m {msg}")
-        else:
-            pass
+            self.color_print(msg, msg_color)
 
-    def lev_info(self, msg: str, msg_color: [int, str]):
+    def lev_info(self, msg: str, msg_color: str) -> None:
+        """
+        Функция вывода сообщений в консоль уровня INFO
+        params msg: сообщение
+        params msg_color: цвет сообщения
+        return
+        """
         if self.log == "info" or self.log == "debug":
-            if msg_color == 7 or msg_color == 'gray':
-                # серый
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[37m {msg}")
-            elif msg_color == 2 or msg_color == 'green':
-                # зеленый green
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[32m {msg}")
-            elif msg_color == 3 or msg_color == 'orange':
-                # оранжевый orange
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[33m {msg}")
-            elif msg_color == 4 or msg_color == 'blue':
-                # синий blue
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[34m {msg}")
-            elif msg_color == 5 or msg_color == 'purple':
-                # фиолетовый purple
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[35m {msg}")
-            elif msg_color == 6 or msg_color == 'skyblue':
-                # голубой blue
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[36m {msg}")
-            else:
-                # черный, если пришел неизвестный аргумент
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[0;0m {msg}")
-        else:
-            pass
+            self.color_print(msg, msg_color)
 
-    def lev_warning(self, msg: str, msg_color: [int, str]) -> None:
+    def lev_warning(self, msg: str, msg_color: str) -> None:
         """
-        red - для неисправности
-        orange - для измерений
-        blue - для управления реле
-        green - для исправности
-        purple - для процедур
-        skyblue - для сухих контактов
-        gray - для основного текста
-        :param msg: str
-        :param msg_color: int, str
-        :return:
+        Функция вывода сообщений в консоль уровня WARNING
+        params msg: сообщение
+        params msg_color: цвет сообщения
+        return
         """
-
         if self.log == "warning" or self.log == "info" or self.log == "debug":
-            if msg_color == 7 or msg_color == 'gray':
-                # серый
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[37m {msg}")
-            elif msg_color == 1 or msg_color == 'red':
-                # красный Red
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[31m {msg}")
-            else:
-                # черный, если пришел неизвестный аргумент
-                print(f"\033[37m [{datetime.time(datetime.now())}] {self.name}: \033[0;0m {msg}")
-        else:
-            pass
+            self.color_print(msg, msg_color)
 
     @staticmethod
-    def progress_bar(percent: int=0, max_it: int=100) -> None:
+    def progress_bar(percent: int = 0, max_it: int = 100) -> None:
         """
         Функция предназначена для отображения в терминале прогресса выполнения.
         :param percent: Текущая итерация
