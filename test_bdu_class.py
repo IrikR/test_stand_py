cf4609b76b9f246ef8e788fbf95e2af28d49ecef
IrikSafin <osiris365@gmail.com>
2023-02-21 00:35:11 +0500

    изменен способ запуска алгоритмов

--- test_bdu_class.py
+++ test_bdu_class.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from new_alg.try_except import TryExcept
 from new_alg.alg_bdu import TestBDU
 
 
 def bdu():
+    try_except = TryExcept()
     test_bdu = TestBDU()
-    test_bdu.full_test_bdu()
-
+    try_except.full_start_test(test_bdu.st_test_bdu, None, 0)
 
 if __name__ == "__main__":
     bdu()
