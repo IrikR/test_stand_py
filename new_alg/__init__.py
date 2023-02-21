e699910cf46d302c6cfeef3e66240aaf8f462b11
IrikSafin <osiris365@gmail.com>
2023-02-21 12:17:35 +0500

    добавлен модуль try_except

--- new_alg/__init__.py
+++ new_alg/__init__.py
@@ -8,4 +8,4 @@ __all__ = ["NAME", "alg_bp", "alg_mtz_5_v4_11", "alg_bdu_014tp", "alg_bki_6_3sh"
            "alg_bzmp_d", "alg_mmtz_d", "alg_bkz_3mk", "alg_bmz_apsh_m", "alg_mtz_5_v2_8", "alg_bdu_1", "alg_bdu_d",
            "alg_btz_t", "alg_bki_1t", "alg_bki_2t", "alg_bzmp_p", "alg_bru_2sr", "alg_bdu_4_2", "alg_bzmp_p1",
            "alg_bu_pmvir", "alg_bdu_4_3", "alg_bdu_1m", "alg_bdu_r_t", "alg_bdu_d4_2", "alg_bdu_dr01", "general_func",
-           "gui", "config"]
+           "gui", "config", "try_except"]
